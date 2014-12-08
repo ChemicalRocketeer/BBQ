96418f75b9b3174182d41425961452993cfe4183
Aaron Suddjian <aasuddjian@gmail.com>
2014-12-08 02:44:09 -0800

    Created some basic underlying architecture
    An ingredient is a basic food element that can be sold out, such as
    pulled pork, cheese, lettuce, bread, etc.
    
    A MenuItem contains one or more ingredients. If any ingredient used in
    the MenuItem is sold out, the whole thing will be considered sold out. A
    MenuItem also keeps track of what it costs, which will be useful when
    planning orders later.
    
    These two classes work together so that at the restaurant when we sell
    out of an ingredient we can press one button which will automatically
    push changes to all affected menu items. Customers, however, will see
    the menu in a way that is easy to read for them.

---
+++ src/com/aptosstbbq/bbqapp/Main.java
@@ -0,0 +1,19 @@
+package com.aptosstbbq.bbqapp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.addIngredient(new Ingredient("Bread"));
+		menu.addIngredient(new Ingredient("Tri-Tip"));
+		menu.addIngredient(new Ingredient("Pulled Pork"));
+		menu.addIngredient(new Ingredient("Cole Slaw"));
+		menu.addIngredient(new Ingredient("Bleu Cheese"));
+		menu.addMenuItem(new MenuItem("Tri-Tip Sandwich", 14.99, menu.getIngredient("Bread"), menu.getIngredient("Tri-Tip")));
+		menu.addMenuItem(new MenuItem("Tri-Tip Meal", 15.99, menu.getIngredient("Bread"), menu.getIngredient("Tri-Tip")));
+		menu.addMenuItem(new MenuItem("Pulled Pork Sandwich", 14.99, menu.getIngredient("Bread"), menu.getIngredient("Pulled Pork")));
+		menu.getIngredient("Pulled Pork").setSoldOut(true);
+		System.out.println(menu.toString());
+	}
+
+}
