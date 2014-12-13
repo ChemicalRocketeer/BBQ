7872c9f757fd696d86bcbce15fa072a7c60c5b34
Aaron Suddjian <aasuddjian@gmail.com>
2014-12-13 13:55:27 -0800

    Added a logger

---
+++ src/com/aptosstbbq/bbqapp/Logger.java
@@ -0,0 +1,26 @@
+package com.aptosstbbq.bbqapp;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class Logger {
+
+	public final String LOG_FILE;
+	
+	public Logger() {
+		this("log.txt");
+	}
+	
+	public Logger(String logFile) {
+		LOG_FILE = logFile;
+	}
+	
+	public void log(String s) {
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(LOG_FILE, true)));
+			out.print(LOG_FILE);
+			out.close();
+		} catch (Exception e) {}
+	}
+}
