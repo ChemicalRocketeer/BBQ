c64cb9599fd62f942a5b980ef9e1391aa7f90b7d
David Aaron Suddjian <aasuddjian@gmail.com>
2014-12-13 16:51:18 -0800

    BigDecimal For Prices
    Gotta get rid of those floating point errors. We're not even doing any
    math on the prices yet but we will be, so better to make the change now.

--- src/com/aptosstbbq/bbqapp/MenuItem.java
+++ src/com/aptosstbbq/bbqapp/MenuItem.java
@@ -1,5 +1,6 @@
 package com.aptosstbbq.bbqapp;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,14 +9,18 @@ public class MenuItem {
 	private String name = "Unnamed Menu Item";
 	private List<Ingredient> ingredients = new ArrayList<>();
 	private List<InterchangableIngredient> interchangableIngredients = new ArrayList<>();
-	private double cost = -1.0;
+	private BigDecimal price = new BigDecimal("-1");
 	
-	public MenuItem(String name, double cost, Ingredient... ings) {
+	public MenuItem(String name, BigDecimal price, Ingredient... ings) {
 		setName(name);
-		setCost(cost);
+		setCost(price);
 		for (Ingredient ing : ings) {
 			addIngredient(ing);
 		}
+	}
+	
+	public MenuItem(String name, String price, Ingredient... args) {
+		this (name, new BigDecimal(price), args);
 	}
 	
 	public boolean isSoldOut() {
@@ -46,11 +51,11 @@ public class MenuItem {
 		}
 	}
 	
-	public double getCost() {
-		return cost;
+	public BigDecimal getPrice() {
+		return price;
 	}
 	
-	public void setCost(double cost) {
-		this.cost = cost;
+	public void setCost(BigDecimal cost) {
+		this.price = cost;
 	}
 }
