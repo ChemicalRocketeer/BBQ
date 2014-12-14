79e138b299600f2c9945efd2735cca3635dc6dc0
aaronmcanerney <aaron.mac016@yahoo.com>
2014-12-13 16:25:54 -0800

    Json implementation
    reads to and from a file

--- src/com/aptosstbbq/bbqapp/Main.java
+++ src/com/aptosstbbq/bbqapp/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Menu menu = new Menu();
+	/*	
 		menu.addIngredient(new Ingredient("Bread"));
 		menu.addIngredient(new Ingredient("Tri-Tip"));
 		menu.addIngredient(new Ingredient("Pulled Pork"));
@@ -14,6 +15,9 @@ public class Main {
 		menu.addMenuItem(new MenuItem("Pulled Pork Sandwich", 14.99, menu.getIngredient("Bread"), menu.getIngredient("Pulled Pork")));
 		menu.getIngredient("Pulled Pork").setSoldOut(true);
 		System.out.println(menu.toString());
+		menu.saveMenu();
+		*/
+		System.out.println(Menu.getMenuFromFile());
 	}
 
 }
