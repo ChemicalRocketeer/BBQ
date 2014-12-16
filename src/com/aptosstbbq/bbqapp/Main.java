843538d0fb07a262775f8391a2d0e7577991e60e
aaronmcanerney <aaron.mac016@yahoo.com>
2014-12-15 16:53:31 -0800

    Added a Jframe
    It is not complete but it is at least a design, methods need to be
    refurbished as there are some issues accessing the other classes

--- src/com/aptosstbbq/bbqapp/Main.java
+++ src/com/aptosstbbq/bbqapp/Main.java
@@ -1,28 +1,23 @@
 package com.aptosstbbq.bbqapp;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Calendar;
+import java.util.EventListener;
+import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class Main {
-
-	public static void main(String[] args) {
-		/*
-		Menu menu = new Menu();
-		menu.addIngredient(new Ingredient("Bread"));
-		menu.addIngredient(new Ingredient("Tri-Tip"));
-		menu.addIngredient(new Ingredient("Pulled Pork"));
-		menu.addIngredient(new Ingredient("Cole Slaw"));
-		menu.addIngredient(new Ingredient("Bleu Cheese"));
-		menu.addMenuItem(new MenuItem("Tri-Tip Sandwich", "14.99", menu.getIngredient("Bread"), menu.getIngredient("Tri-Tip")));
-		menu.addMenuItem(new MenuItem("Tri-Tip Meal", "15.99", menu.getIngredient("Bread"), menu.getIngredient("Tri-Tip")));
-		menu.addMenuItem(new MenuItem("Pulled Pork Sandwich", "14.99", menu.getIngredient("Bread"), menu.getIngredient("Pulled Pork")));
-		menu.getIngredient("Pulled Pork").setSoldOut(true);
-		System.out.println(menu.toString());
-		menu.saveMenu();
-		*/
-		Logger log = new Logger();
-		String msg = "Ran at " + Calendar.getInstance().getTime().toString();
-		log.log(msg);
-		System.out.println(msg);
+public class Main{
+	public static void main(String[] args){
+		Frame jframe = new Frame();
 		System.out.println(Menu.getMenuFromFile());
+		
 	}
 }
