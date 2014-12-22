18da44e2aeeafdd0427e97598f3c8d7e4b60a069
David Aaron Suddjian <aasuddjian@gmail.com>
2014-12-22 14:38:24 -0800

    Reading the menu from the web

--- src/com/aptosstbbq/bbqapp/Main.java
+++ src/com/aptosstbbq/bbqapp/Main.java
@@ -3,13 +3,11 @@ package com.aptosstbbq.bbqapp;
 import com.aptosstbbq.bbqapp.menu.Menu;
 import com.aptosstbbq.bbqapp.menu.MenuItem;
 import com.aptosstbbq.bbqapp.web.FTPOut;
+import com.aptosstbbq.bbqapp.web.HTTPIn;
 
 public class Main {
 	public static void main(String[] args) {
-		String menuJSON = Utils.readFile("menu.json");
-		Menu menu = Menu.fromJSON(menuJSON);
-		menu.addMenuItem(new MenuItem("Pulled Pork Meal", "14", menu.getIngredient("Pulled Pork")));
-		menu.toggleSoldOut("Pulled Pork");
-		System.out.println("Menu written to FTP Server: " + new FTPOut().write(menu.toJSON()));
+		Menu menu = Menu.fromJSON(new HTTPIn().read());
+		System.out.println(menu.toString());
 	}
 }
