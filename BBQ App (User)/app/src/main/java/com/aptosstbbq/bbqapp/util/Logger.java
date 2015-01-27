cae4e6b0401f8101fc89e80d5d60920ed2af4e77
David Aaron Suddjian <aasuddjian@gmail.com>
2015-01-27 00:50:35 -0800

    Logger prefix

--- BBQ App (User)/app/src/main/java/com/aptosstbbq/bbqapp/util/Logger.java
+++ BBQ App (User)/app/src/main/java/com/aptosstbbq/bbqapp/util/Logger.java
@@ -2,12 +2,14 @@ package com.aptosstbbq.bbqapp.util;
 
 public class Logger {
 
+	public static final Logger ERROR = new Logger("error");
 	public static final Logger DEFAULT = new Logger("log");
 	public static final Logger MENU_CHANGES = new Logger("menu_changes");
 	public static final Logger SELL_OUT = new Logger("sell_out");
 	public static final Logger WEB = new Logger("web");
 
-	private static final String FILE_EXTENSION = ".log";
+	private static final String PATH_PREFIX = "/log";
+	private static final String PATH_SUFFIX = ".txt";
 	
 	private final String LOG_FILE;
 	
@@ -19,7 +21,7 @@ public class Logger {
 	 * @param logFile
 	 */
 	private Logger(String logFile) {
-		LOG_FILE = logFile + FILE_EXTENSION;
+		LOG_FILE = PATH_PREFIX + logFile + PATH_SUFFIX;
 	}
 
 	public void log(String s) {
