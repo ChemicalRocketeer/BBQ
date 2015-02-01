9d25d9959f2acebacad4269bb1b5edcc3e35db90
David Aaron Suddjian <aasuddjian@gmail.com>
2015-02-01 00:16:58 -0800

    import bbq essentials and et project going

--- BBQUserApp/app/src/main/java/com/davidaaronsyndicate/aptosstbbq/MenuDisplay.java
+++ BBQUserApp/app/src/main/java/com/davidaaronsyndicate/aptosstbbq/MenuDisplay.java
@@ -1,27 +1,39 @@
-package com.aptosstbbq.bbquserapp;
+package com.davidaaronsyndicate.aptosstbbq;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.aptosstbbq.bbqapp.menu.BBQMenu;
+import com.aptosstbbq.bbqapp.menu.Ingredient;
+
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MenuDisplay extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu_display);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_menu_display, menu);
         return true;
     }
+    private void addMenuLevelOne(BBQMenu menu){
+        LinearLayout screen = (LinearLayout) findViewById(R.id.MenuLayout1);
+        List<Ingredient> ings = menu.getIngredients();
+        for(Ingredient ing : ings){
 
+        }
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
